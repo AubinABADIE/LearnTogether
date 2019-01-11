41c86c8f82c27b3e8ae8de6904b6dc6c889f16f6
Unknown <mariesalelles@gmail.com>
2019-01-11 11:02:05 +0100

    MyServerRoomDAO create room

--- src/client/Room/RoomServices.java
+++ src/client/Room/RoomServices.java
@@ -12,7 +12,13 @@ public class RoomServices {
     }
 
     /**
-     * This method creates String with all room attributes and send it to the server
+     *  This method creates String with all room attributes and send it to the server
+     * @param name : room name
+     * @param capacity : room capacity
+     * @param building : room building number
+     * @param hasProjector : if there is a projeector in the room
+     * @param hasComputer : if there are computers in the room
+     * @param description : small description of the room
      */
     public void handleCreateRoom (String name,int capacity, int building, boolean hasProjector, boolean hasComputer, String description){
         String objConstruct = name + "-/-" + capacity + "-/-" + building + "-/-" + hasProjector + "-/-" + hasComputer + "-/-" + description;
